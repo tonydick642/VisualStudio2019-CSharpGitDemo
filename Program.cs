ba121d5904fc0b56f92457216606a515c2b1969d
Tony Dick <tonydick642@gmail.com>
2020-09-17 20:18:12 -0500

    change to the program

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace VisualStudio2019_CSharpGitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("here is the change!");
         }
     }
 }
